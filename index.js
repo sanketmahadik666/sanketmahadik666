03a54245319a6a628e773cf91a00b199296a2fa1
sanketmahadik666 <mahadiks666@gmail.com>
2025-06-07 18:24:02 +0530

    Initial commit: Portfolio website

---
+++ index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded",function () {
+
+    const github = document.getElementById("github");
+    github.innerHTML = '<a href = "https://github.com/sanketmahadik666" target ="_blank">@Sanket666</a>';
+
+    const leetcode = document.getElementById("leetcode");
+    leetcode.innerHTML = '<a href="https://leetcode.com/u/SanketMahadik/" target="_blank">@lonaAlen</a>';
+
+
+    const project1GitHub = document.querySelector("#project1 .github-repository-link");
+    project1GitHub.innerHTML = '<a href="https://github.com/sanketmahadik666/ExpenseMonitoringSwing--1-" target="_blank">GitHub Repository</a>';
+
+
+    const project3GitHub = document.querySelector("#project3 .github-repository-link");
+    project3GitHub.innerHTML = '<a href="https://github.com/sanketmahadik666/ADCET-HACKTHON" target="_blank">GitHub Repository</a>';
+
+
+})
